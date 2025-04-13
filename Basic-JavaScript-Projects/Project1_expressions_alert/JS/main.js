a6bc2598566a9f0f2404b2af357eb17f849e236b
ALTeriorMotive <lightning.aaron1221@gmail.com>
2025-04-12 21:27:24 -0500

    Event challenge

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -3,3 +3,11 @@ var Sent1 = 'This is the beginning of the string', Sent2 = ' and this is the end
 window.alert(5+2); /* Creates an alert on the page */
 
 document.write(Sent1 + Sent2); /* Writes on the document */
+
+function initialColor(){
+    document.body.style.backgroundColor = 'aliceblue';
+}
+
+function colorChange(){
+    document.body.style.backgroundColor = 'cadetblue';
+}
