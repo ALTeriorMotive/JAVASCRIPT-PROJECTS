33dce1c8632e6dfe926955442e7e12f9e391a580
ALTeriorMotive <lightning.aaron1221@gmail.com>
2025-04-16 23:51:04 -0500

    Comments

--- Basic-JavaScript-Projects/Project4_dictionaries/JS/main.js
+++ Basic-JavaScript-Projects/Project4_dictionaries/JS/main.js
@@ -1,5 +1,5 @@
 function stuff(){
-    var avengers = {
+    var avengers = { /* Dictionary */
         0: "Chris Evans - Captain America",
         1: "Robert Downey Jr - Iron Man",
         2: "Chris Hemsworth - Thor",
@@ -7,7 +7,22 @@ function stuff(){
         4: "Scarlet Johansson - Black Widow",
         5:"Jeremy Renner - Hawkeye"
     };
-    var randomNum = String(Math.floor(Math.random() * 6));
-    document.getElementById('Dictionary').innerHTML = "Here's an actor in the MCU: " + avengers[randomNum];
+    var randomNum = String(Math.floor(Math.random() * 6)); /* Creates a random number between 0 and 5 to be used later */
+    delete avengers[randomNum]; /* Deletes the item with the given random key */
+    document.getElementById('Dictionary').innerHTML = "Here's an actor in the MCU: " + avengers[randomNum]; /* Replaces text in 'Dictionary' element with the value that matches a random key */
+    console.log(randomNum);
+}
+
+function stuff2(){
+    var avengers = { /* Dictionary */
+        0: "Chris Evans - Captain America",
+        1: "Robert Downey Jr - Iron Man",
+        2: "Chris Hemsworth - Thor",
+        3: "Mark Ruffalo - Hulk",
+        4: "Scarlet Johansson - Black Widow",
+        5:"Jeremy Renner - Hawkeye"
+    };
+    var randomNum = String(Math.floor(Math.random() * 6)); /* Creates a random number between 0 and 5 to be used later */
+    document.getElementById('Dictionary2').innerHTML = "Here's an actor in the MCU: " + avengers[randomNum]; /* Replaces text in 'Dictionary2' element with the value that matches a random key */
     console.log(randomNum);
 }
