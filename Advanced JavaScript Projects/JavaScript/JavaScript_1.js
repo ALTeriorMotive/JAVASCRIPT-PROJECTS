0b408dcecd5aaf7dd38c3a310d348eb01ec3fa2e
ALTeriorMotive <lightning.aaron1221@gmail.com>
2025-04-26 09:33:38 -0500

    Advanced stuff 1

--- Advanced JavaScript Projects/JavaScript/JavaScript_1.js
+++ Advanced JavaScript Projects/JavaScript/JavaScript_1.js
@@ -19,3 +19,21 @@ function select(){
     }
     document.getElementById('response').innerHTML = response;
 }
+
+function wipe(){
+    var clear = document.getElementsByClassName('wipe');
+    for(i=0; i<clear.length; i++){
+        clear[i].innerHTML = '';
+        clear[i].style.listStyleType = 'none';
+    }
+}
+
+var circle = document.getElementById('canvas').getContext("2d");
+
+var gradient = circle.createLinearGradient(0, 0, 500, 500);
+gradient.addColorStop(0, 'blue');
+gradient.addColorStop(0.5, 'purple');
+gradient.addColorStop(0.95, 'black');
+
+circle.fillStyle = gradient;
+circle.fillRect(0, 0, 500, 500);
